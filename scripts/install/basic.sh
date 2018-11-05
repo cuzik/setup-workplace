2bf12425b34a46c3b2027807e6e081606f0b9ac5
cuzik <cuzikjack@gmail.com>
2018-11-05 16:30:04 -0200

    add some packages

--- scripts/install/basic.sh
+++ scripts/install/basic.sh
@@ -1,3 +1,39 @@
-# Install terminator
+#!/bin/zsh
 
-sudo apt install terminator
+#functions logger
+
+return_logger() {
+  if [ "$1" -ne "0" ]; then
+    echo "\e[93m<<< \e[31m[failed]\e[33m[$2]\e[36m[code:$1]\e[39m"
+  else
+    echo "\e[93m<<< \e[32m[succesfuly]\e[33m[$2]\e[36m[code:$1]\e[39m"
+  fi
+}
+
+install_by_apt() {
+  # echo "\e[93m>>> \e[95mInstalling \e[94m[$1]\e[39m"
+  sudo apt-get -qq -y install $1 > /dev/null
+  return_logger $? $1
+}
+
+update() {
+  sudo apt-get -qq -y update > /dev/null
+  return_logger $? update
+}
+
+
+update
+
+package_names=(
+  terminator
+  wget
+  git
+  cmake
+  carlos
+  libpq-dev
+  golang-go
+)
+
+for package_name in $package_names; do
+  install_by_apt $package_name
+done
