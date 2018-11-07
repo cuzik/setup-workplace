f2fd427041bb73dc766211ad64d2fde9244d9427
cuzik <cuzikjack@gmail.com>
2018-11-07 14:02:48 -0200

    add more things to script

--- scripts/install/basic.sh
+++ scripts/install/basic.sh
@@ -31,8 +31,38 @@ add_ppa() {
   return_logger $? $1
 }
 
-title() {
+add_all_ppa() {
+  for ppa in $@; do
+    add_ppa $ppa
+  done
+}
+
+notify() {
   echo "\n\e[91m>\e[92m>\e[93m>\e[94m>\e[95m>\e[96m>\e[92m $1 \e[96m<\e[95m<\e[94m<\e[93m<\e[92m<\e[91m<\e[39m\n"
+}
+
+downlaod() {
+  echo "\e[93m>>> \e[35mDownload \e[34m[$1]\e[39m"
+  wget $1 -nv -P /tmp/boot/ > /dev/null
+  return_logger $? $1
+}
+
+downlaod_all_deb() {
+  for link in $@; do
+    downlaod $link
+  done
+}
+
+install_by_deb_file() {
+  echo "\e[93m>>> \e[35mInstalling \e[34m[$1]\e[39m"
+  sudo dpkg -i /tmp/boot/$1 > /dev/null
+  return_logger $? $1
+}
+
+install_all_deb_files() {
+  for deb_file in $@; do
+    install_by_deb_file $deb_file
+  done
 }
 
 native_package_names=(
@@ -46,22 +76,44 @@ native_package_names=(
 
 external_package_names=(
   peek
+  rvm
 )
 
-title "Updating"
+ppa_paths=(
+  peek-developers/stable
+  rael-gc/rvm
+)
+
+links_to_deb=(
+  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  https://az764295.vo.msecnd.net/stable/7f3ce96ff4729c91352ae6def877e59c561f4850/code_1.28.2-1539735992_amd64.deb
+)
+
+deb_files=(
+  google-chrome-stable_current_amd64.deb
+  code_1.28.2-1539735992_amd64.deb
+)
+
+notify "Updating"
 update
 
-title "Native Package"
+notify "Native Package"
 install_all_package $native_package_names
 
-title "Updating"
+notify "Updating"
 update
 
-title "Add PPAs"
-add_ppa peek-developers/stable
+notify "Add PPAs"
+add_all_ppa $ppa_paths
 
-title "Updating"
+notify "Updating"
 update
 
-title "External Package"
+notify "External Package"
 install_all_package $external_package_names
+
+notify "Dowloading all .deb"
+downlaod_all_deb $links_to_deb
+
+notify "Instaling .deb"
+install_all_deb_files $deb_files
--- scripts/install/vscode.sh
+++
@@ -1,7 +0,0 @@
-# see https://code.visualstudio.com/docs/?dv=linux64_deb
-
-mkdir ../../download
-cd ../../download
-wget https://az764295.vo.msecnd.net/stable/7f3ce96ff4729c91352ae6def877e59c561f4850/code_1.28.2-1539735992_amd64.deb
-sudo dpkg -i code_1.28.2-1539735992_amd64.deb
-cd ..
